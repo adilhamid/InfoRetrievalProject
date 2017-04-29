6ca59ba4501e10d2627a74fdb7b75ebf8c365e17
Sumeet Arora <singh.sumeet210191@gmail.com>
2017-04-28 19:42:18 -0500

    removing unwanted js files and bioler plate for integration

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask
 from flask import Flask, render_template
 from flask import Flask, request, send_from_directory
 import os
+import algorithm_wrapper
 app = Flask(__name__,static_folder='templates')
 @app.route("/")
 def main():
    	print os.getcwd()
    	return render_template('index.html')
+
+@app.route("/entity", methods=['POST'])
+def entity():
+    _entity = request.form['searchfield']
+    answer = algorithm_wrapper.triviaAlgorithm(_entity)
+    for key in answer:
+        return key
+
 
 @app.route('/js/<path:path>')
 def send_js(path):
