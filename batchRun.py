ca295e5140ca8c690916b424f169415df95a20cc
ADIL HAMID MALLA <adilhamidmalla@ADILs-MacBook-Pro.local>
2017-04-29 12:35:12 -0500

    Updated the batchRun Code

--- batchRun.py
+++ batchRun.py
@@ -1,6 +1,6 @@
 import algorithm_wrapper
-import wikipedia as wiki
-import pdb
+import wiki_parser
+import wiki_trivia_metric_calculator
 import sys
 import os
 
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     # Read the each line of the entityList and Run the Trivia Algorithm
     if os.path.isfile(file_path):
         open_entityFile = open(file_path, "r")
+        wiki_parser_instance = wiki_parser.WikiParser()
+        wiki_trivia_metric_calculator_instance = wiki_trivia_metric_calculator.WikiTriviaMetricCalculator()
         for entity in open_entityFile:
-            algorithm_wrapper.triviaAlgorithm(entity)
+            algorithm_wrapper.triviaAlgorithm(entity,wiki_parser_instance,wiki_trivia_metric_calculator_instance)
         open_entityFile.close()
