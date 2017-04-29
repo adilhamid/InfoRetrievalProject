4b05fc3c75d5e6c0d4db57f7a79681b6b764a057
Sumeet Arora <singh.sumeet210191@gmail.com>
2017-04-29 03:20:43 -0500

    Added the input file and updated the test run

--- algorithm_test.py
+++ algorithm_test.py
@@ -9,10 +9,8 @@ if __name__ == "__main__":
     wiki_parser_instance = wiki_parser.WikiParser()
     wiki_trivia_metric_calculator_instance = wiki_trivia_metric_calculator.WikiTriviaMetricCalculator()
     print "Init done"
-
-    while True:
-        print "Enter Some Entity"
-        input_entity = raw_input()
-        if input_entity == "1":
-            break
-        print algorithm_wrapper.triviaAlgorithm(input_entity, wiki_parser_instance, wiki_trivia_metric_calculator_instance)
+    target = open("input.txt", "r")
+    for line in target:
+        line = line.replace('\n', '')
+        print algorithm_wrapper.triviaAlgorithm(line, wiki_parser_instance, wiki_trivia_metric_calculator_instance)
+    target.close()
